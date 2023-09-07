4140c3ff90db1d243b1ab4e37a61ae839c3c5d17
na0nie <naonchun@gmail.com>
2023-09-07 11:26:10 -0700

    favicon

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useEffect, useState } from "react";
+import Head from "next/head";
 import Nav from "@/components/Nav";
 import axios from "axios";
 import BarChart from "@/components/BarChart";
@@ -77,32 +78,45 @@ export default function Home() {
 
   useEffect(() => {
     axios
-      .get("https://api.sheety.co/2996d79e2117ff0d746768a9b29ec03c/cityEmployeePayroll2017June2023/cityEmployeePayroll2017June2023Csv", {
-        params: {
-          $limit: 110000,
-        },
-      })
+      .get(
+        "https://api.sheety.co/2996d79e2117ff0d746768a9b29ec03c/cityEmployeePayroll2017June2023/cityEmployeePayroll2017June2023Csv",
+        {
+          params: {
+            $limit: 110000,
+          },
+        }
+      )
       .then((response) => {
-        const data = response.data.cityEmployeePayroll2017June2023Csv;        ;
+        const data = response.data.cityEmployeePayroll2017June2023Csv;
         console.log("payroll", data);
         const payrollData = data
-        .filter((x: any) => x.payYear === 2023)
-        .map((x: any) => ({
-          id: x.id,
-          record: x.recordNbr,
-          fName: x.firstName,
-          lName: x.lastName,
-          jobTitle: x.jobTitle,
-          department: x.departmentTitle,
-          basePay: x.regularPay.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ','),
-          overtimePay: x.overtimePay.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ','),
-          otherPay: x.allOtherPay.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ','),
-          personalBenefits: x.benefitPay.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ','),
-          totalPay: x.totalPay.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ','),
-        }));
+          .filter((x: any) => x.payYear === 2023)
+          .map((x: any) => ({
+            id: x.id,
+            record: x.recordNbr,
+            fName: x.firstName,
+            lName: x.lastName,
+            jobTitle: x.jobTitle,
+            department: x.departmentTitle,
+            basePay: x.regularPay
+              .toFixed(2)
+              .replace(/\B(?=(\d{3})+(?!\d))/g, ","),
+            overtimePay: x.overtimePay
+              .toFixed(2)
+              .replace(/\B(?=(\d{3})+(?!\d))/g, ","),
+            otherPay: x.allOtherPay
+              .toFixed(2)
+              .replace(/\B(?=(\d{3})+(?!\d))/g, ","),
+            personalBenefits: x.benefitPay
+              .toFixed(2)
+              .replace(/\B(?=(\d{3})+(?!\d))/g, ","),
+            totalPay: x.totalPay
+              .toFixed(2)
+              .replace(/\B(?=(\d{3})+(?!\d))/g, ","),
+          }));
 
-      setPayroll(payrollData);
-    })
+        setPayroll(payrollData);
+      })
       .catch((error) => {
         console.error("Error:", error);
       });
@@ -117,6 +131,66 @@ export default function Home() {
         withGlobalStyles
         withNormalizeCSS
       >
+        <Head>
+          <meta charSet="utf-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+          />
+          <title>LAPD Dashboard</title>
+          <meta property="og:type" content="website" />
+          <meta name="twitter:site" content="LAPD Dashboard" />
+          <meta name="twitter:creator" content="@lacontroller" />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta
+            name="twitter:title"
+            key="twittertitle"
+            content="LAPD Dashboard"
+          ></meta>
+          <meta
+            name="twitter:description"
+            key="twitterdesc"
+            content="LAPD Dashboard"
+          ></meta>
+          <meta
+            name="twitter:image"
+            key="twitterimg"
+            content="https://lapd-dashboard.vercel.app/dashboard.png"
+          ></meta>
+          <meta name="description" content="LAPD Dashboard" />
+
+          <meta
+            property="og:url"
+            content="https://lapd-dashboard.vercel.app/"
+          />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="LAPD Dashboard" />
+          <meta property="og:description" content="LAPD Dashboard" />
+          <meta
+            property="og:image"
+            content="https://lapd-dashboard.vercel.app/dashboard.png"
+          />
+
+          <link rel="icon" type="image/x-icon" href="/favicon/favicon.ico" />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon/cropped-favicon-1-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="180x180"
+            href="/favicon/cropped-favicon-1-180x180.png"
+          />
+          <meta name="msapplication-TileColor" content="#ffffff" />
+          <meta
+            name="msapplication-config"
+            content="/favicon/browserconfig.xml"
+          />
+          <meta name="theme-color" content="#ffffff" />
+        </Head>
         <Nav />
         <h1 className="mt-5 font-bold text-2xl text-white">LAPD Dashboard</h1>
         <h2 className="mt-7 font-bold text-xl text-white">
@@ -135,7 +209,7 @@ export default function Home() {
         <h2 className="mt-7 font-bold text-xl text-white">LAPD Staffing</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 md:gap-6">
           <Staffing />
-          <Payroll payroll={payroll}/>
+          <Payroll payroll={payroll} />
         </div>
         <h2 className="mt-7 font-bold text-xl text-white">LAPD Arrests</h2>
         <div className="mt-2">
@@ -152,7 +226,9 @@ export default function Home() {
           <Audits />
         </div>
         <h2 className="mt-7 font-bold text-xl text-white">Ongoing Audits</h2>
-        <h2 className="mt-7 font-bold text-xl text-white">Transparency Facts</h2>
+        <h2 className="mt-7 font-bold text-xl text-white">
+          Transparency Facts
+        </h2>
       </MantineProvider>
     </section>
   );
