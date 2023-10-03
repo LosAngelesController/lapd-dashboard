7e1ac9a8239b09c6bcd655b4ab3e46ad4a1f9034
na0nie <naonchun@gmail.com>
2023-10-03 11:32:04 -0700

    staffing update & twitter

--- components/Staffing.tsx
+++ components/Staffing.tsx
@@ -8,48 +8,48 @@ Chart.register(...registerables);
 
 function Staffing() {
   const years = [
-    {
-      year: 2017,
-      officers: 10322,
-      civilianEmployees: 3457
-    },
+    // {
+    //   year: 2017,
+    //   officers: 10322,
+    //   civilianEmployees: 3457
+    // },
     {
       year: 2018,
-      officers: 10379,
-      civilianEmployees: 4432
+      officers: 9990,
+      civilianEmployees: 3002
     },
     {
       year: 2019,
-      officers: 10377,
-      civilianEmployees: 3746
+      officers: 10004,
+      civilianEmployees: 2992
     },
     {
       year: 2020,
-      officers: 10217,
-      civilianEmployees: 3795
+      officers: 9963,
+      civilianEmployees: 3070
     },
     {
       year: 2021,
-      officers: 9988,
-      civilianEmployees: 3429
+      officers: 9390,
+      civilianEmployees: 2749
     },
     {
       year: 2022,
-      officers: 9742,
-      civilianEmployees: 3366
+      officers: 9277,
+      civilianEmployees: 2689
     },
-    {
-      year: 2023,
-      officers: 9378,
-      civilianEmployees: 3018
-    },
+    // {
+    //   year: 2023,
+    //   officers: 9378,
+    //   civilianEmployees: 3018
+    // },
   ];
 
   var data = {
     labels: years.map((x: any) => x.year),
     datasets: [
       {
-        label: "SWORN OFFICERS",
+        label: "SWORN PERSONNEL",
         data: years.map((x: any) => x.officers),
         backgroundColor: [
           "#41ffca",
@@ -61,7 +61,7 @@ function Staffing() {
         borderWidth: 1,
       },
       {
-        label: "CIVILIAN STAFF",
+        label: "CIVILIAN PERSONNEL",
         data: years.map((x: any) => x.civilianEmployees),
         backgroundColor: [
           "#f5e942",
@@ -110,7 +110,7 @@ function Staffing() {
 
   return (
     <div className="mt-4">
-      <h5 className="mb-2">SWORN LAPD OFFICERS FY2012 - JUNE 2023</h5>
+      <h5 className="mb-2">SWORN LAPD OFFICERS 2018 - 2022</h5>
       <Bar data={data} height={325} options={options} />
     </div>
   );
--- components/Twitter.tsx
+++ components/Twitter.tsx
@@ -47,6 +47,207 @@ export default function Twitter() {
         </blockquote>
         <script async src="https://platform.twitter.com/widgets.js"></script>
       </div>
+      <div className="mt-4 flex justify-center">
+        <blockquote className="twitter-tweet" data-lang="en" data-width="500">
+          <p lang="en" dir="ltr">
+            🚨 On Wed 8/23, LA City Council will vote on the NEW LAPD contract.
+            <br />
+            <br />
+            According to the CAO’s numbers, the proposed contract will cost
+            taxpayers approximately $994 million over the next 4 years ($123M
+            this fiscal year).
+            <br />
+            <br />
+            (Does not include overtime &amp; applies to Lieutenants &amp; below)
+          </p>
+          &mdash; LA City Controller Kenneth Mejia (@lacontroller){" "}
+          <a href="https://twitter.com/lacontroller/status/1693837756928331834?ref_src=twsrc%5Etfw">
+            August 22, 2023
+          </a>
+        </blockquote>
+        <script async src="https://platform.twitter.com/widgets.js"></script>
+      </div>
+      <div className="mt-4 flex justify-center">
+        <blockquote className="twitter-tweet" data-lang="en" data-width="500">
+          <p lang="en" dir="ltr">
+            Our response to LAPD’s statement on our arrest map/analysis:
+            <br />
+            <br />
+            Our work relied on LAPD’s only comprehensive, publicly available
+            arrest data, which they update weekly on the City’s/Mayor&apos;s
+            Open Data Portal.
+            <br />
+            <br />
+            This is not the Mayor’s data, this is LAPD’s data.
+            <a href="https://t.co/QYOs9LqBCI">https://t.co/QYOs9LqBCI</a>
+            <a href="https://t.co/ACVzybue3l">pic.twitter.com/ACVzybue3l</a>
+          </p>
+          &mdash; LA City Controller Kenneth Mejia (@lacontroller)
+          <a href="https://twitter.com/lacontroller/status/1686797150158729216?ref_src=twsrc%5Etfw">
+            August 2, 2023
+          </a>
+        </blockquote>
+        <script async src="https://platform.twitter.com/widgets.js"></script>
+      </div>
+      <div className="mt-4 flex justify-center">
+        <blockquote className="twitter-tweet" data-lang="en" data-width="500">
+          <p lang="en" dir="ltr">
+            Before City Council votes on the new LAPD contract, the public
+            deserves more than 72 hrs notice.
+            <br />
+            <br />
+            Over the years, the police budget has increased while recruitment
+            trends down & crime fluctuates regardless of staffing #&apos;s.
+            <br />
+            <br />
+            The public deserves to know the full long-term costs.
+            <a href="https://t.co/nq3jzMNVQR">https://t.co/nq3jzMNVQR</a>
+          </p>
+          &mdash; LA City Controller Kenneth Mejia (@lacontroller)
+          <a href="https://twitter.com/lacontroller/status/1686525752064557063?ref_src=twsrc%5Etfw">
+            August 1, 2023
+          </a>
+        </blockquote>
+        <script async src="https://platform.twitter.com/widgets.js"></script>
+      </div>
+      <div className="mt-4 flex justify-center">
+        <blockquote className="twitter-tweet" data-lang="en" data-width="500">
+          <p lang="en" dir="ltr">
+            In recent years, 78% of arrests were of Black & Hispanic/Latino
+            people, even though Black & Hispanic/Latino people make up only 56%
+            of LA’s general population.
+            <br />
+            <br />
+            See more arrest data in our LAPD Arrests map:
+            <a href="https://t.co/gwKxTPwp0U">https://t.co/gwKxTPwp0U</a>
+            <a href="https://t.co/kkpoCUvkfh">https://t.co/kkpoCUvkfh</a>
+          </p>
+          &mdash; LA City Controller Kenneth Mejia (@lacontroller)
+          <a href="https://twitter.com/lacontroller/status/1684665052547461121?ref_src=twsrc%5Etfw">
+            July 27, 2023
+          </a>
+        </blockquote>
+        <script async src="https://platform.twitter.com/widgets.js"></script>
+      </div>
+      <div className="mt-4 flex justify-center">
+        <blockquote className="twitter-tweet" data-lang="en" data-width="500">
+          <p lang="en" dir="ltr">
+            📱NEW Map & Analysis:
+            <br />
+            Over the past four years (2019-2022), LAPD has made nearly 300,000
+            arrests. We created a map of these arrests:
+            <a href="https://t.co/VMjUKLo6Hm">https://t.co/VMjUKLo6Hm</a>
+            <br />
+            <br />
+            Select a year, then filter the map by race, Council District, LAPD
+            Division, & type of arrest.
+            <a href="https://t.co/Es9B0MzlOm">pic.twitter.com/Es9B0MzlOm</a>
+          </p>
+          &mdash; LA City Controller Kenneth Mejia (@lacontroller)
+          <a href="https://twitter.com/lacontroller/status/1684303167721459713?ref_src=twsrc%5Etfw">
+            July 26, 2023
+          </a>
+        </blockquote>
+        <script async src="https://platform.twitter.com/widgets.js"></script>
+      </div>
+      <div className="mt-4 flex justify-center">
+        <blockquote className="twitter-tweet" data-lang="en" data-width="500">
+          <p lang="en" dir="ltr">
+            43% of all 41.18 arrests from Jan 2012 to May 2023 are of Black
+            people.
+            <br />
+            Black people are 8% of the population in Los Angeles.
+            <br />
+            <br />
+            See our map and analysis to help understand how homelessness is
+            criminalized in the City of LA:
+            <a href="https://t.co/nT5HuVOPVJ">https://t.co/nT5HuVOPVJ</a>
+            <a href="https://t.co/vW7PQToGHp">pic.twitter.com/vW7PQToGHp</a>
+          </p>
+          &mdash; LA City Controller Kenneth Mejia (@lacontroller)
+          <a href="https://twitter.com/lacontroller/status/1674276685737492481?ref_src=twsrc%5Etfw">
+            June 29, 2023
+          </a>
+        </blockquote>
+        <script async src="https://platform.twitter.com/widgets.js"></script>
+      </div>
+      <div className="mt-4 flex justify-center">
+        <blockquote className="twitter-tweet" data-lang="en" data-width="500">
+          <p lang="en" dir="ltr">
+            Yes, you can go to jail for being homeless in Los Angeles.
+            <br />
+            <br />
+            Our Director of Public Safety breaks down misdemeanors and
+            infractions. 84% of all 41.18 arrests from 2012-May 2023 are
+            misdemeanors, which can lead to jail time.
+            <br />
+            <br />
+            Visit our 41.18 arrests map:
+            <a href="https://t.co/nT5HuVOPVJ">https://t.co/nT5HuVOPVJ</a>
+            <a href="https://t.co/NbUz26eQpQ">pic.twitter.com/NbUz26eQpQ</a>
+          </p>
+          &mdash; LA City Controller Kenneth Mejia (@lacontroller)
+          <a href="https://twitter.com/lacontroller/status/1674098123671764992?ref_src=twsrc%5Etfw">
+            June 28, 2023
+          </a>
+        </blockquote>
+        <script async src="https://platform.twitter.com/widgets.js"></script>
+      </div>
+      <div className="mt-4 flex justify-center">
+        <blockquote className="twitter-tweet" data-lang="en" data-width="500">
+          <p lang="en" dir="ltr">
+            Last week, we released a map and analysis of LAPD arrests related to
+            LAMC 41.18 which criminalizes sitting, lying, &amp; sleeping, or
+            placing personal property in the public right-of-way in certain
+            instances.
+            <br />
+            <br />
+            Please see the map and analysis below:
+            <a href="https://t.co/NFzBd3roDq">https://t.co/NFzBd3roDq</a>
+          </p>
+          &mdash; LA City Controller Kenneth Mejia (@lacontroller)
+          <a href="https://twitter.com/lacontroller/status/1673901978941005824?ref_src=twsrc%5Etfw">
+            June 28, 2023
+          </a>
+        </blockquote>
+        <script async src="https://platform.twitter.com/widgets.js"></script>
+      </div>
+      <div className="mt-4 flex justify-center">
+        <blockquote className="twitter-tweet" data-lang="en" data-width="500">
+          <p lang="en" dir="ltr">
+            Tomorrow (Tues 9/27), the LA Police Commission will vote on a
+            donation of a $10,000 police dog from a controversial kennel that
+            shares its name with a piece of Nazi history.
+            <br />
+            <br />
+            Agenda Item No. 4D
+            <br />
+            Agenda:{" "}
+            <a href="https://t.co/Q0XoPD04QN">https://t.co/Q0XoPD04QN</a>
+          </p>
+          &mdash; LA City Controller Kenneth Mejia (@lacontroller)
+          <a href="https://twitter.com/lacontroller/status/1673461201496576000?ref_src=twsrc%5Etfw">
+            June 26, 2023
+          </a>
+        </blockquote>
+        <script async src="https://platform.twitter.com/widgets.js"></script>
+      </div>
+      <div className="mt-4 flex justify-center">
+        <blockquote className="twitter-tweet" data-lang="en" data-width="500">
+          <p lang="en" dir="ltr">
+            Black people have been disproportionately arrested for violating a
+            city ordinance that critics say is used to punish the unhoused,
+            according to a new report released by the Los Angeles City
+            Controller’s Office.{" "}
+            <a href="https://t.co/6mkO3b9nxU">https://t.co/6mkO3b9nxU</a>
+          </p>
+          &mdash; KTLA (@KTLA){" "}
+          <a href="https://twitter.com/KTLA/status/1672032787354755072?ref_src=twsrc%5Etfw">
+            June 23, 2023
+          </a>
+        </blockquote>
+        <script async src="https://platform.twitter.com/widgets.js"></script>
+      </div>
     </>
   );
 }
